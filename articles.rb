4f3ec775e0744fc8f7abfc4d2fc86d5251a878ac
Pavan Sarguru <pavansarguru@gmail.com>
2016-12-17 16:42:25 -0600

    create method to add user

--- articles.rb
+++ articles.rb
@@ -8,7 +8,7 @@
 #       topic
 #       rating
 #       comment
-#       view all saved articles with all info
+#      view all saved articles with all info
 # 
 #   Stretch Features:
 #     go straight to articles from terminal
@@ -29,23 +29,46 @@
 #       author_name
 #       topic
 #       url
-# 
+
+
+require "sqlite3"
+require "faker"
+require_relative "db_initialize"
+
+db = SQLite3::Database.open("db_articles.db")
+
 # Methods:
 #   ADD USER
 #     Input: name
 #     Steps: 
 #       run SQL command to add new user
 #     output: new user in database
+def add_user(db, name)
+  db.execute("INSERT INTO user (name) VALUES (?)", [name])
+end
+
 #   ADD ARTICLE
 #     Input: name, article_name, author_name, topic, url, stars, comments
 #     Steps:
 #       run SQL command to add new article with attributes
 #     Output: new article in database
+
+def add_article(db, name, article_name, author_name, topic, url, stars, comments)
+  db.execute("INSERT INTO article (article_name, author_name, topic, url) VALUES (?, ?, ?, ?)",
+    [article_name, author_name, topic, url])
+  db.execute("INSERT INTO review (user_id, article_id, stars, comments) VALUES (?, ?, ?, ?)",
+    [find_user_id(db,name), find_article_id(db,article), stars, comments]) 
+end 
+
 #   DELETE ARTICLE
 #     Input: name, article_name
 #     Steps:
 #       run SQL command to delete article by name
 #     Output: article removed from database
+def delete_article(db, name, article_name)
+  db.execute("DELETE FROM article WHERE article_name=(?) and user_id = (?)",
+    [article_name, find_user_id(db, name)])
+end
 #   CHANGE REVIEW OF ARTICLE
 #     Input: name, article_name, stars, comments
 #     Steps:
@@ -61,3 +84,15 @@
 #     Steps:
 #       Run Launchy command to open url
 #     Output: opened website in separate window
+
+
+# Driver Code
+
+article_data = db.execute("SELECT * FROM article")
+review_data = db. execute("SELECT * FROM review")
+
+# Test Adding user
+add_user(db, "Pavan")
+
+p db.execute("SELECT * FROM user")
+
--- db_initialize.rb
+++ db_initialize.rb
@@ -1,8 +1,8 @@
 # Run ruby code to initialize database values
 # Serves as record of commands used to create existing databases
-
 require "sqlite3"
 require "faker"
+  
 
 # create SQLite3 database
 db = SQLite3::Database.new("db_articles.db")
@@ -45,53 +45,28 @@ SQL
 
 db.execute(create_table_review)
 
-# Functions to push values
+def find_user_id (db, user)
+  user_values = db.execute("SELECT id FROM user where name = ?", [user])
+end
+
+def find_article_id (db, article)
+  article_values = db.execute("SELECT id FROM article where article_name = ?", [article])
+end
+
+# Method to push values
 def create_values(db, name, article, author, topic, url, stars, comments)
   db.execute("INSERT INTO user (name) VALUES (?)", [name])
-  db.execute("INSERT INTO article (article_name, author_name, topic, url) VALUES (?, ?, ?, ?)", [article, author, topic, url])
+  db.execute("INSERT INTO article (article_name, author_name, topic, url) VALUES (?, ?, ?, ?)", 
+    [article, author, topic, url])
+  db.execute("INSERT INTO review (user_id, article_id, stars, comments) VALUES (?, ?, ?, ?)",
+    [find_user_id(db,name), find_article_id(db, article), stars, comments])
 end
 
-user_values = db.execute("SELECT * FROM user")
-# p user_values
-article_values = db.execute("SELECT * FROM article") 
-# p article_values
 
-user_id = []
-
-def find_user_id (user)
-  db = SQLite3::Database.new("db_articles.db")
-  user_values = db.execute("SELECT * FROM user")
-  user_id = 0
-
-  user_values.each do |user_array|
-    if user_array[1] == user
-      user_id = user_array[0]
-    end
-  end 
-  user_id
-end
-
-p find_user_id("Lincoln Dach")
-
-def find_article_id (article)
-  db = SQLite3::Database.new("db_articles.db")
-  article_values = db.execute("SELECT * FROM article")
-  article_id = 0
-
-  article_values.each do |article_array|
-    if article_array[1] == article
-      article_id = article_array[0]
-    end
-  end 
-  article_id
-end
-
-p find_article_id("That Hideous Strength")
-
-
+# Populates with random data
 # 3.times do 
-#   create_values(db, Faker::Name.name, Faker::Book.title, Faker::Book.author, Faker::Book.genre, Faker::Internet.url, 3, Faker::Hipster.sentence(4, false, 4))
+#   create_values(db, Faker::Name.name, Faker::Book.title, Faker::Book.author, Faker::Book.genre, 
+#     Faker::Internet.url, 3, Faker::Hipster.sentence(4, false, 4))
 # end
-
 
 
